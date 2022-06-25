b9b9d8fe41717c9e6e4440e3fb510014c276de2a
Gabriel Gomide <gogomide.go@gmail.com>
2022-06-25 08:40:31 -0300

    header att

--- portfolio-website/src/components/header/CTA.jsx
+++ portfolio-website/src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/Gabriel_OliveiraGomide_Currículo2021.pdf'
 const CTA = () => {
     return (
         <div className="cta">
-            <a href={CV} download>Download CV</a>
-            <a href="#contact">Entre em contato</a>
+            <a href={CV} download className="btn">Download CV</a>
+            <a href="#contact" className="btn btn-primary">Entre em contato</a>
         </div>
     )
 }
--- portfolio-website/src/components/header/Header.jsx
+++ portfolio-website/src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import me from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +13,13 @@ const Header = () => {
         <h5 className="text-light">Frontend Developer</h5>
 
         <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={me} alt="me" />
+        </div>
+        
+        <a href="#contact" className='scroll__down'>Arraste para baixo</a>
       </div>
     </header>
   )
