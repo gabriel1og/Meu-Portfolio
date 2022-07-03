caa5a7006c58c0790b948c013d266d4ed7909b6f
Gabriel Gomide <gogomide.go@gmail.com>
2022-07-03 12:20:57 -0300

    portfolio section creation

--- portfolio-website/src/components/portfolio/Portfolio.jsx
+++ portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './portfolio.css'
+import p1 from '../../assets/p1.webp'
+import p2 from '../../assets/p2.webp'
+import p3 from '../../assets/p3.webp'
+import p4 from '../../assets/p4.webp'
+import p5 from '../../assets/p5.webp'
+import p6 from '../../assets/p6.webp'
+
+const data = [
+  {
+    id: 1,
+    image: p1,
+    title: 'Orion UI kit - Charts templates & infographics in Figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18254672-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 2,
+    image: p2,
+    title: 'Eclipse - Figma dashboard UI kit for data design web apps',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18474796-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+  },
+  {
+    id: 3,
+    image: p3,
+    title: 'Eclipse - Figma dashboard UI kit for data design web apps',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18474819-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+  },
+  {
+    id: 4,
+    image: p4,
+    title: 'Orion UI kit - Charts templates & infographics in Figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18542932-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 5,
+    image: p5,
+    title: 'Orion UI kit - Charts templates & infographics in Figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18543062-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 6,
+    image: p6,
+    title: 'Orion UI kit - Charts templates & infographics in Figma',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shots/18543078-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+        </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
