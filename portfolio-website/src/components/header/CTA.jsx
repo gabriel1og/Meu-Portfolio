aef8ad6d1906eb51fb244bfb2e315a7f1f45bbc7
Gabriel Gomide <76018617+gabriel1og@users.noreply.github.com>
2023-01-23 09:07:37 -0300

    add cv to header

--- portfolio-website/src/components/header/CTA.jsx
+++ portfolio-website/src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CV from '../../assets/Gabriel_OliveiraGomide_Currículo-2023.pdf'
+import CV from '../../assets/myCurriculum.pdf'
 
 const CTA = () => {
     return (
