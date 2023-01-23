512c6970ff06b322800c0a16623e37e0a402f71d
Gabriel Gomide <gogomide.go@gmail.com>
2023-01-23 10:45:28 -0300

    small changes at image display/style

--- portfolio-website/src/components/about/About.jsx
+++ portfolio-website/src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import eu from '../../assets/eu.jpeg'
+import eu from '../../assets/me.png'
 import { FaAward } from 'react-icons/fa'
 import { FiUsers } from 'react-icons/fi'
 import { VscFolderLibrary } from 'react-icons/vsc'
