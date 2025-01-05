cef82573cc023a822f4494770d751d9a218e4711
gabriel1og <gogomide.go@gmail.com>
2025-01-05 05:01:37 -0300

    creating scroll util file

---
+++ src/utils/scrollBySections.ts
@@ -0,0 +1,17 @@
+export interface Sections {
+  SOBRE: "sobre";
+  EXPERIÊNCIA: "experiência";
+  PROJETOS: "projetos";
+  CONTATO: "contato";
+}
+
+export const scrollTo = (sectionId: string) => {
+  const element = document.getElementById(sectionId);
+  if (element) {
+    const yOffset = window.scrollY + element.getBoundingClientRect().top;
+    window.scrollTo({
+      top: yOffset - 140,
+      behavior: "smooth",
+    });
+  }
+};
