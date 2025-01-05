0a3bdf6886af2b3de41b2977cedb428358dc3c8b
gabriel1og <gogomide.go@gmail.com>
2025-01-05 05:02:25 -0300

    updating page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
+import TopButton from "@/components/Generics/TopButton";
 import Projects from "@/components/Projects";
 import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex flexDirection="column" mx="auto" px={4} gap={20}>
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      mx="auto"
+      px={{ base: 6, md: 4 }}
+    >
+      <TopButton />
       <About />
       <Experience />
       <Projects />
