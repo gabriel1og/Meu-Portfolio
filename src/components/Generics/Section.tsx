b84278baaf2fc95ddc866943bf95392af9600712
gabriel1og <gogomide.go@gmail.com>
2025-01-05 22:25:19 -0300

    adding animation on each section

--- src/components/Generics/Section.tsx
+++ src/components/Generics/Section.tsx
@@ -1,5 +1,6 @@
 import { Flex, Heading, StackProps, VStack } from "@chakra-ui/react";
 import { JSX } from "react";
+import { InView } from "react-intersection-observer";
 
 export default function Section({
   sectionId,
@@ -13,34 +14,33 @@ export default function Section({
   props?: StackProps;
 }) {
   return (
-    <Flex
-      className="section"
-      w="100%"
-      maxW="1200px"
-      h="100%"
-      py="5rem"
-      id={sectionId}
-    >
-      <VStack
-        mx="auto"
-        w="100%"
-        maxW="800px"
-        justifyContent="center"
-        {...props}
-      >
-        <Heading
-          fontSize="1.4rem"
-          fontWeight={700}
-          letterSpacing=".1em"
-          textAlign="center"
-          textTransform="uppercase"
-          bgClip="text"
-          bgGradient="linear-gradient(to right, #3a21f3, #085aff,#2362cf, #44bbee)"
-        >
-          {title}
-        </Heading>
-        {children}
-      </VStack>
+    <Flex w="100%" maxW="1200px" h="100%" py="8rem" id={sectionId}>
+      <InView triggerOnce threshold={0.1}>
+        {({ inView, ref }) => (
+          <VStack
+            mx="auto"
+            w="100%"
+            maxW="800px"
+            justifyContent="center"
+            {...props}
+            ref={ref}
+            className={`section ${inView ? "scrolled" : ""}`}
+          >
+            <Heading
+              fontSize="1.4rem"
+              fontWeight={700}
+              letterSpacing=".1em"
+              textAlign="center"
+              textTransform="uppercase"
+              bgClip="text"
+              bgGradient="linear-gradient(to right, #3a21f3, #085aff,#2362cf, #44bbee)"
+            >
+              {title}
+            </Heading>
+            {children}
+          </VStack>
+        )}
+      </InView>
     </Flex>
   );
 }
